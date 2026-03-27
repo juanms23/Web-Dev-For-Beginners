5a55c11f21467c1397cbdf0749d33fcd042e70ea
Juan Carlos Muratalla-Sanchez <murataj2@wwu.edu>
2026-03-27 13:16:29 -0700

    Working on arrays and loops

---
+++ 2-js-basics/4-arrays-loops/arrays-and-loops.js
@@ -0,0 +1,32 @@
+/**
+ * Lists every 3rd number between 1-20 and prints to the console
+ */
+const numberPatternGenerator = () => {
+  const pattern = [];
+  // starts at 3 and increments by 3 until it reaches 20
+  for (let i = 3; i < 20; i += 3) {
+    pattern.push(i);
+  }
+  console.log(pattern.join(", "));
+};
+
+/**
+ * Finds the maximum value in an array of numbers
+ * @param {numbers[]} numbers - The array of numbers to search through
+ * @returns {number} The highest number in the array 
+ */
+const findMaximum = (numbers) => {
+
+}
+
+const findMinimum = (numbers) => {
+
+}
+
+const calculateSum = (numbers) => {
+
+}
+
+const secondHighest = (numbers) => {
+
+}
